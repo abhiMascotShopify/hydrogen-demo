9356886cceb7a02f2939f95dcc8442e31fa446cd
abhijit@mstindia.com <abhijit@mstindia.com>
2024-03-01 11:30:23 +0530

    new

--- app/routes/($locale).collections._index.jsx
+++ app/routes/($locale).collections._index.jsx
@@ -54,7 +54,7 @@ function CollectionsGrid({collections}) {
 function CollectionItem({collection, index}) {
   return (
     <Link
-      className="collection-item"
+      className="collection-item border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       key={collection.id}
       to={`/collections/${collection.handle}`}
       prefetch="intent"
