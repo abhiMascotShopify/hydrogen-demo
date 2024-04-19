2093c65f015753c4c7f455a3c2d0ff36b6d04d3c
abhijit@mstindia.com <abhijit@mstindia.com>
2024-04-19 11:11:43 +0530

    footer

--- app/components/YouTubeVideo.jsx
+++ app/components/YouTubeVideo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import {useMediaQuery} from 'react-responsive';
 
-const MobileVideo = ({playVideo}) => {
-  const embedUrl = `https://www.youtube.com/embed/TUeHBWEZr0A`;
+const MobileVideo = ({playVideo,src}) => {
   return (
     <div className="w-full h-full mx-auto">
       <div className="relative rounded-lg" cursor-pointer>
-        <img className='relative rounded-2xl' src="https://cdn.shopify.com/s/files/1/0809/4253/0882/files/youtube_thumbnail.jpg?v=1712992484" alt="Youtube Video"></img>
+        <img className='relative rounded-2xl' onClick={()=> playVideo() } src={src} alt="Youtube Video"></img>
         <img className='absolute top-[50%] left-[50%]' src="/youtube_icon.png" />
         {/* <iframe
           className="rounded-lg w-full h-60"
@@ -19,12 +18,11 @@ const MobileVideo = ({playVideo}) => {
   );
 };
 
-const DesktopVideo = ({playVideo}) => {
-  const embedUrl = `https://www.youtube.com/embed/TUeHBWEZr0A`;
+const DesktopVideo = ({playVideo,src}) => {
   return (
     <div className="w-4/5 mx-auto">
       <div className="relative rounded-2xl p-10 cursor-pointer">
-        <img className="rounded-2xl relative" onClick={()=> playVideo() } src="https://cdn.shopify.com/s/files/1/0809/4253/0882/files/youtube_thumbnail.jpg?v=1712992484" alt="Youtube Video"></img>
+        <img className="rounded-2xl relative" onClick={()=> playVideo() } src={src} alt="Youtube Video"></img>
         <img className='absolute top-[50%] left-[50%]' src="/youtube_icon.png" />
         {/* <iframe
           title="YouTube Video"
@@ -39,7 +37,8 @@ const DesktopVideo = ({playVideo}) => {
 
 const YouTubeVideo = () => {
   //const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-  const embedUrl = `https://www.youtube.com/embed/TUeHBWEZr0A`;
+  const embedUrl = `https://www.youtube.com/embed/WKKai8jures?si=RwowF2iZZmbeL8H3" `;
+  const src = "https://cdn.shopify.com/s/files/1/0809/4253/0882/files/youtube_thumbnail.jpg?v=1713503453" 
   const isLargeScreen = useMediaQuery({minWidth: 1024});
   const isSmall = useMediaQuery({maxWidth: 640});
 
@@ -49,8 +48,8 @@ const YouTubeVideo = () => {
 
   return (
     <div>
-      {isLargeScreen && <DesktopVideo playVideo={playVideo}></DesktopVideo>}
-      {isSmall && <MobileVideo playVideo={playVideo}></MobileVideo>}
+      {isLargeScreen && <DesktopVideo playVideo={playVideo} src={src}></DesktopVideo>}
+      {isSmall && <MobileVideo playVideo={playVideo} src={src}></MobileVideo>}
     </div>
   );
 };
