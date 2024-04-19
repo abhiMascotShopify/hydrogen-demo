05acb9a5d3cea9c29d0994890f94aecda4a08f40
abhijit@mstindia.com <abhijit@mstindia.com>
2024-04-19 13:35:03 +0530

    footer

--- app/components/LooksComp.jsx
+++ app/components/LooksComp.jsx
@@ -3,7 +3,7 @@ const LooksComp = ({looks})=>{
     return(
         <div className="looks">
             <h2 className="uppercase text-center"> L.I.T : Looks In Trend </h2>
-            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 px-4 sm:px-20">
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-4 px-0 sm:px-20">
                 {
                     products.map((product)=>{
                         return(
